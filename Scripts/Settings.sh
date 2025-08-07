6befe7ccf8d5a83acaeee34cef2a792bd5d8d3c5
277598414 <277598414@qq.com>
2025-08-07 11:37:10 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -49,14 +49,14 @@ if [[ "${WRT_TARGET^^}" == *"QUALCOMMAX"* ]]; then
 	echo "CONFIG_FEED_nss_packages=n" >> ./.config
 	echo "CONFIG_FEED_sqm_scripts_nss=n" >> ./.config
 	#开启sqm-nss插件
-	echo "CONFIG_PACKAGE_luci-app-sqm=y" >> ./.config
-	echo "CONFIG_PACKAGE_sqm-scripts-nss=y" >> ./.config
+	echo "CONFIG_PACKAGE_luci-app-sqm=n" >> ./.config
+	echo "CONFIG_PACKAGE_sqm-scripts-nss=n" >> ./.config
 	#设置NSS版本
-	echo "CONFIG_NSS_FIRMWARE_VERSION_11_4=n" >> ./.config
+	echo "CONFIG_NSS_FIRMWARE_VERSION_11_4=y" >> ./.config
 	if [[ "${WRT_CONFIG,,}" == *"ipq50"* ]]; then
-		echo "CONFIG_NSS_FIRMWARE_VERSION_12_2=y" >> ./.config
+		echo "CONFIG_NSS_FIRMWARE_VERSION_12_2=n" >> ./.config
 	else
-		echo "CONFIG_NSS_FIRMWARE_VERSION_12_5=y" >> ./.config
+		echo "CONFIG_NSS_FIRMWARE_VERSION_12_5=n" >> ./.config
 	fi
 	#无WIFI配置调整Q6大小
 	if [[ "${WRT_CONFIG,,}" == *"wifi"* && "${WRT_CONFIG,,}" == *"no"* ]]; then
