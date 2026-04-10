81bb45adf81ec5153f1a6ae5226c2089e9e90965
277598414 <277598414@qq.com>
2026-04-10 11:43:33 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -2,7 +2,6 @@
 # SPDX-License-Identifier: MIT
 # Copyright (C) 2026
 
-#!/bin/bash
 
 #移除luci-app-attendedsysupgrade
 sed -i "/attendedsysupgrade/d" $(find ./feeds/luci/collections/ -type f -name "Makefile")
